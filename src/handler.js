8d76c9ffc0fe48e09b8be859582213b5b80c2b28
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-11-04 17:00:09 +0800

    feat: handler get book by id

--- src/handler.js
+++ src/handler.js
@@ -82,4 +82,26 @@ const getAllBooksHandler = () => ({
   },
 });
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const book = books.filter((n) => n.id === bookId)[0];
+
+  if (book === undefined) {
+    const response = h.response({
+      status: "fail",
+      message: "Buku tidak ditemukan",
+    });
+    response.code(404);
+    return response;
+  }
+
+  return {
+    status: "success",
+    data: {
+      books,
+    },
+  };
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
