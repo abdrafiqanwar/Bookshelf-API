e46834892a1d27bc385ca0044585aec56b486951
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-11-05 14:35:49 +0800

    fix: message id not found

--- src/handler.js
+++ src/handler.js
@@ -146,7 +146,7 @@ const editBookByIdHandler = (request, h) => {
 
   const response = h.response({
     status: "fail",
-    message: "Gagal memperbarui buku. Mohon isi nama buku",
+    message: "Gagal memperbarui buku. Id tidak ditemukan",
   });
   response.code(404);
   return response;
